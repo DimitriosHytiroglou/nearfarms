64bf1fe39d215911465d9571825ac18bb6fa3b5a
DimitriosHytiroglou <DimitriosHytiroglou@users.noreply.github.com>
2018-04-11 19:39:35 -0700

    Added file upload functionality. Need to fix filepaths

--- app/views.py
+++ app/views.py
@@ -1,13 +1,21 @@
+import os
 from flask import render_template, redirect, request
 from app import app, models
 from .forms import ProductForm
 from .models import *
+from werkzeug import secure_filename
 # Access the models file to use SQL functions
 
 
 @app.route('/')
 def index():
-    return redirect('/farmer_home')
+    return redirect('/home')
+    #return redirect('/farmer_home')
+
+@app.route('/home', methods=['GET'])
+def home():
+    
+    return render_template('home.html')
 
 
 @app.route('/add_product', methods=['GET', 'POST'])
@@ -41,3 +49,43 @@ def farmer_home():
         productList.append(product)
 
     return render_template('farmer.html', productList=productList)
+
+@app.route('/consumer_home', methods=['GET'])
+def consumer_home():
+    
+    pass
+
+    #UNCOMMENT when template is created
+    # return render_template('consumer.html')
+
+@app.route('/file_upload', methods = ['GET', 'POST'])
+def file_upload():
+
+    UPLOAD_FOLDER = '/file_uploads/'
+    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+    def allowed_file(filename):
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+### NEED TO FIX ALL THE RETURN FIELDS IN THE FOLLOWING CODE
+
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return '''<!doctype html><p>BOB</p>'''#redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return '''<!doctype html><p>BOB</p>''' #redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.root_path, filename))
+            #file.save(os.path.join(UPLOAD_FOLDER, filename))
+            return '''<!doctype html><p>BOB</p>'''#redirect(url_for('uploaded_file',
+                                    #filename=filename))
+    
