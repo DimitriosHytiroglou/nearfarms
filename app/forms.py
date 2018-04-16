61b0e785c8ae59d4baba2abd274477ef07070956
bobrandt <31392557+bobrandt@users.noreply.github.com>
2018-04-16 14:16:13 -0700

    updated with small changes

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, IntegerField, SelectField, PasswordField, validators
 from flask_wtf.html5 import EmailField
+from flask_wtf.file import FileField
 from wtforms.validators import DataRequired
 #from .model import findUsers, chooseCollection
 
@@ -51,7 +52,7 @@ class ProductForm(Form):
     subType = StringField('subType', validators=[DataRequired()])
     quantity = IntegerField('quantity', validators=[DataRequired()])
     price = IntegerField('price', validators=[DataRequired()])			 # probably need to change this to float
-    image = StringField('image', validators=[DataRequired()]) 			 # this is just a placeholder for now
+    image = FileField('image', validators=[DataRequired()]) 			 # this is just a placeholder for now
 
 
 
--- app/static/main.js
+++ app/static/main.js
@@ -1,3 +1,5 @@
+
+// update products in line via farmer.html
 $(".update_col").on('click', function() {
 	var product = $(this).closest('tr').find('.product_col').text();
 	var type = $(this).closest('tr').find('.type_col').text();
@@ -6,7 +8,7 @@ $(".update_col").on('click', function() {
 	var price = $(this).closest('tr').find('.price_col').text();
 	var image = $(this).closest('tr').find('.image_col').text();
 	var tr = $(this).closest("tr").remove().clone();
-	$('#product_table').append("<tr> <td class='product_col'><input value="+product+"></td> <td class='type_col'><input value="+type+"></td> <td class='subtype_col'><input type="+subtype+"/></td> <td class='quantity_col'><input type="+quantity+"/></td> <td class='price_col'><input type="+price+"/></td> <td class='image_col'><input type="+image+"/></td> <button class='submit_button'> Submit</button></a></td> </tr>");
+	$('#product_table').append("<tr> <td class='product_col'><input value="+product+"></td> <td class='type_col'><input value="+type+"></td> <td class='subtype_col'><input type="+subtype+"/></td> <td class='quantity_col'><input type="+quantity+"/></td> <td class='price_col'><input type="+price+"/></td> <td class='image_col'><input type="+image+"/></td> <td class='submit_col'> <button class='submit_button'> Submit</button></td> </tr>");
 
 });
 
